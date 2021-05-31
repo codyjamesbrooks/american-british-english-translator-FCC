7d3af36f19315db0b9cec508d13f7d4972af194b
codyjamesbrooks <71521423+codyjamesbrooks@users.noreply.github.com>
2021-05-31 09:53:59 -0600

    added american to british translation logic

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,43 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const { restart } = require("nodemon");
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+  americanToBritish(text) {
+    let translatedText = (" " + text).slice(1);
 
+    // translate time
+    translatedText = translatedText.replace(
+      /([01][1-9]):([0-6][0-9])/g,
+      this.highlightText("$1.$2")
+    );
+
+    let translationArray = [
+      americanOnly,
+      americanToBritishSpelling,
+      americanToBritishTitles,
+    ];
+
+    for (const translationObjet of translationArray) {
+      for (const [wordToTranslate, wordAfterTranslate] of Object.entries(
+        translationObjet
+      )) {
+        let wordToTranslateRegex = new RegExp(wordToTranslate, "gi");
+        translatedText = translatedText.replace(
+          wordToTranslateRegex,
+          this.highlightText(wordAfterTranslate)
+        );
+      }
+    }
+
+    return translatedText;
+  }
+
+  highlightText(text) {
+    return "<span class='highlight'>" + text + "</span>";
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,21 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const { restart } = require("nodemon");
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    console.log(req.body);
+
+    if (!req.body.text) {
+      return res.json({ error: "No text to translate" });
+    }
+
+    if (req.body.locale === "american-to-british") {
+      let translatedText = translator.americanToBritish(req.body.text);
+      return res.json({ translation: translatedText });
+    }
+  });
 };
